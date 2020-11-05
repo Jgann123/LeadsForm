1654c010192be4e230e0c1c5f188db2e163ff6ef
Jeremy Gann <gannjeremy644@gmail.com>
2020-11-04 19:22:41 -0500

    Connected

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxkgWotCMyvIKoT9aGtPCndZdHtHrq8i972t9CKAVjO6xC7T0I/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwqy4AtMwM-cEfQjmYsJz0SOSXxYdjg4-M5CLV_X2glDFkVbeO2/exec'
 const form = document.forms['google-sheet']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .then(response => alert("Thanks! You have successfully submitted your lead ..."))
     .catch(error => console.error('Error!', error.message))
 })
+
