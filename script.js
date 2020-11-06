b6a1ab94d7c77d40344029fcd749d9c0280d9b1f
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2020-11-06 14:01:18 -0500

    adding sheets

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwqy4AtMwM-cEfQjmYsJz0SOSXxYdjg4-M5CLV_X2glDFkVbeO2/exec'
 const form = document.forms['google-sheet']
+const scriptURL2 = 'https://script.google.com/macros/s/AKfycbx_vvpE4LJDQ1P__e-fmHl46QOJKR1DXja6l8UMgiEGlzgN8EcT/exec'
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => alert("Thanks! You have successfully submitted your lead ..."))
     .catch(error => console.error('Error!', error.message))
+    
 })
+
+var droDownList = document.getElementById('list');
+
+
+function sortList(dropDownList) {
+  if (droDownList == "Bev Smith Kia’s Reprocessed Car Sale & Trade In Event") {
+    return
+    form.addEventListener('submit', i => {
+      i.preventDefault()
+      fetch(scriptURL2, { method: 'POST', body: new FormData(form)})
+        .then(response => alert("Thanks! You have successfully submitted your lead ..."))
+        .catch(error => console.error('Error!', error.message))
+        
+    })
+  }
+}
+
+sortList();
+
+
+
+
+
+
+
+
 
